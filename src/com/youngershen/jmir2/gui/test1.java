8486ba86eed758266bc275d31eaee7107a9ca4bb
younger.shen <younger.x.shen@gmail.com>
2012-10-28 19:10:17 +0800

    init the project

---
+++ src/com/youngershen/jmir2/gui/test1.java
@@ -0,0 +1,21 @@
+/**
+ * author : younger.shen 
+ * email  : younger.x.shen@gmail.com
+ * blog   : http://www.youngershen.com
+ * 2012-10-28
+ * TODO
+ */
+package com.youngershen.jmir2.gui;
+
+/**
+ * @author youngershen
+ *
+ */
+public class test1 {
+
+	public static void main(String[] args){
+		
+		
+		
+	}
+}
